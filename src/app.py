3dcf97c9a182bd345e89fd8bcbf146288ea43680
chinapandaman <benjaminli801@gmail.com>
2020-08-06 21:17:19 +0000

    chocolate app route

--- src/app.py
+++ src/app.py
@@ -6,4 +6,5 @@ from bottle.bottle import Bottle
 class ChocolateApp(object):
     def __init__(self, app_name):
         self.app = Bottle()
+        self.route = self.app.route
         self.app.mount("/{}".format(app_name), self.app)
