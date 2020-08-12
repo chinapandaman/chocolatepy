7f8136b2d16727bcc33d8dc7be8eec6e747e72e3
chinapandaman <b.j.li@foxmail.com>
2020-08-11 21:56:56 -0500

    better run chocolate server

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from bottle.bottle import Bottle
+from bottle.bottle import Bottle, run
 
 
 class ChocolateServer(object):
@@ -9,3 +9,6 @@ class ChocolateServer(object):
 
     def register_app(self, chocolate_app):
         self.server.merge(chocolate_app.app)
+
+    def run(self):
+        run(self.server, host="localhost", port=8080)
