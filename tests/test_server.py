ed4d6b660d00545ccecf091d6ff0ac67332080b4
chinapandaman <benjaminli801@gmail.com>
2020-08-14 04:52:27 +0000

    test app, test server

---
+++ tests/test_server.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from chocolatepy import ChocolateApp, ChocolateServer
+from webtest import TestApp
+
+
+@pytest.fixture
+def app_one():
+    app = ChocolateApp("one")
+
+    @app.route("/")
+    def index():
+        return "one"
+
+    return app
+
+
+@pytest.fixture
+def app_two():
+    app = ChocolateApp("two")
+
+    @app.route("/")
+    def index():
+        return "two"
+
+    return app
+
+
+@pytest.fixture
+def app_three():
+    app = ChocolateApp("three")
+
+    @app.route("/")
+    def index():
+        return "three"
+
+    return app
+
+
+def test_register_apps(app_one, app_two, app_three):
+    server = ChocolateServer()
+
+    server.register_apps(app_one, app_two, app_three)
+
+    app = TestApp(server.server)
+
+    assert app.get("/one").status == "200 OK"
+    assert app.get("/one").text == "one"
+
+    assert app.get("/two").status == "200 OK"
+    assert app.get("/two").text == "two"
+
+    assert app.get("/three").status == "200 OK"
+    assert app.get("/three").text == "three"
+
+    assert app.get("/one").text == app.get("/").text
+
+
+def test_register_apps_with_default_app(app_one, app_two, app_three):
+    server = ChocolateServer()
+
+    server.register_apps(app_one, app_two, app_three, default_app=app_two)
+
+    app = TestApp(server.server)
+
+    assert app.get("/two").text == app.get("/").text
