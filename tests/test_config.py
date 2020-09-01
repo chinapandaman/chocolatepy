8fa82afb2b801d3092d88f8d2cfdf44c9ef04710
chinapandaman <b.j.li@foxmail.com>
2020-09-01 16:48:44 -0500

    test case sensitive config

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,9 +8,11 @@ def test_set_config():
 
     assert app_config.set_config("s1", "k1", "v1")
     assert app_config.set_config("s2", "k2", "v2")
+    assert app_config.set_config("S1", "K1", "V1")
 
     assert app_config.env.environ["app_one.s1.k1"] == "v1"
     assert app_config.env.environ["app_one.s2.k2"] == "v2"
+    assert app_config.env.environ["app_one.S1.K1"] == "V1"
 
 
 def test_get_config():
@@ -18,9 +20,11 @@ def test_get_config():
 
     app_config.set_config("s1", "k1", "v1")
     app_config.set_config("s2", "k2", "v2")
+    app_config.set_config("S1", "K1", "V1")
 
     assert app_config.get_config("s1", "k1") == "v1"
     assert app_config.get_config("s2", "k2") == "v2"
+    assert app_config.get_config("S1", "K1") == "V1"
 
     assert not app_config.get_config("s5", "k5")
 
@@ -30,5 +34,10 @@ def test_config_as_dict():
 
     app_config.set_config("s1", "k1", "v1")
     app_config.set_config("s2", "k2", "v2")
+    app_config.set_config("S1", "K1", "V1")
 
-    assert app_config.as_dict() == {"s1": {"k1": "v1"}, "s2": {"k2": "v2"}}
+    assert app_config.as_dict() == {
+        "s1": {"k1": "v1"},
+        "s2": {"k2": "v2"},
+        "S1": {"K1": "V1"},
+    }
