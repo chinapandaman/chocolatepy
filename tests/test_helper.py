b24ac9f4492f059af71268c85fc95c27b11dfe61
chinapandaman <b.j.li@foxmail.com>
2020-09-03 18:34:13 -0500

    test helper decode token

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 
+import time
+from datetime import datetime, timedelta
+
+import jwt
 import pytest
 from chocolatepy import ChocolateApp, ChocolateServer
 from helper import ChocolateHelper
@@ -47,3 +51,35 @@ def test_current_app_name(app_one, app_two):
     assert app.get("/two").status == "200 OK"
 
     assert ChocolateHelper().current_app_name() == app_two.name
+
+
+def test_decode_token():
+    token = ""
+
+    assert (
+        ChocolateHelper().decode_token(token, "secret", ["HS256"]) == "Invalid token."
+    )
+
+    payload = {
+        "exp": datetime.utcnow() + timedelta(seconds=1),
+        "iat": datetime.utcnow(),
+        "sub": 4,
+    }
+
+    token = jwt.encode(payload, "secret", "HS256")
+
+    time.sleep(5)
+
+    assert (
+        ChocolateHelper().decode_token(token, "secret", ["HS256"]) == "Token expired."
+    )
+
+    payload = {
+        "exp": datetime.utcnow() + timedelta(seconds=3600),
+        "iat": datetime.utcnow(),
+        "sub": 5,
+    }
+
+    token = jwt.encode(payload, "secret", "HS256")
+
+    assert ChocolateHelper().decode_token(token, "secret", ["HS256"]) == 5
